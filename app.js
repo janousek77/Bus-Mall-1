cd852552e7c2a1a0f43f92633027f28c4935ada7
Zachary Diehl <zachary.diehl@gmail.com>
2017-03-20 13:42:30 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var imgArr = []
+imgArr[0] = 'assets/bag.jpg';
+imgArr[1] = 'assets/banana.jpg';
+imgArr[2] = 'assets/bathroom.jpg';
+imgArr[3] = 'assets/boots.jpg';
+imgArr[4] = 'assets/breakfast.jpg';
+imgArr[5] = 'assets/bubblegum.jpg';
+imgArr[6] = 'assets/chair.jpg';
+imgArr[7] = 'assets/cthulhu.jpg';
+imgArr[8] = 'assets/dog-duck.jpg';
+imgArr[9] = 'assets/dragon.jpg';
+imgArr[10] = 'assets/pen.jpg';
+imgArr[11] = 'assets/pet-sweep.jpg';
+imgArr[12] = 'assets/scissors.jpg';
+imgArr[13] = 'assets/shark.jpg';
+imgArr[14] = 'assets/sweep.jpg';
+imgArr[15] = 'assets/tauntaun.jpg';
+imgArr[16] = 'assets/unicorn.jpg';
+imgArr[17] = 'assets/usb.jpg';
+imgArr[18] = 'assets/water-can.jpg';
+imgArr[19] = 'assets/wine-glass.jpg';
+
+console.log(imgArr);
