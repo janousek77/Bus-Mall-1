1167a0b6e58b03cd5afdf5a8562fddfa8c2fa4c8
Zachary Diehl <zachary.diehl@gmail.com>
2017-03-20 18:52:10 -0700

    Completed constructor function and populating random images.  I am still trying to get images to display onto HTML page

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
 'use strict';
 
-var imgArr = []
-imgArr[0] = 'assets/bag.jpg';
-imgArr[1] = 'assets/banana.jpg';
-imgArr[2] = 'assets/bathroom.jpg';
-imgArr[3] = 'assets/boots.jpg';
-imgArr[4] = 'assets/breakfast.jpg';
-imgArr[5] = 'assets/bubblegum.jpg';
-imgArr[6] = 'assets/chair.jpg';
-imgArr[7] = 'assets/cthulhu.jpg';
-imgArr[8] = 'assets/dog-duck.jpg';
-imgArr[9] = 'assets/dragon.jpg';
-imgArr[10] = 'assets/pen.jpg';
-imgArr[11] = 'assets/pet-sweep.jpg';
-imgArr[12] = 'assets/scissors.jpg';
-imgArr[13] = 'assets/shark.jpg';
-imgArr[14] = 'assets/sweep.jpg';
-imgArr[15] = 'assets/tauntaun.jpg';
-imgArr[16] = 'assets/unicorn.jpg';
-imgArr[17] = 'assets/usb.jpg';
-imgArr[18] = 'assets/water-can.jpg';
-imgArr[19] = 'assets/wine-glass.jpg';
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
 
-console.log(imgArr);
+var imgArr = [];
+var currentImg = [];
+var previousImg = [];
+
+function img(name, source) {
+  this.name = name;
+  this.source = source;
+  imgArr.push(this);
+};
+
+function random() {
+  return Math.floor(Math.random() * imgArr.length);
+};
+
+function randomImg() {
+  while(currentImg.length < 3) {
+    var imgSelector = imgArr[random()];
+    if(!currentImg.includes(imgSelector) && !previousImg.includes(imgSelector)) {
+      currentImg.push(imgSelector);
+    }
+  }
+  var firstImg = imgArr[currentImg[0]].source;
+  var secondImg = imgArr[currentImg[1]].source;
+  var thirdImg = imgArr[currentImg[2]].source;
+  document.getElementById('img1').src = firstImg;
+  document.getElementById('img2').src = secondImg;
+  document.getElementById('img3').src = thirdImg;
+};
+
+var bag = new img('bag', 'assets/bag.jpg');
+var banana = new img('banana', 'assets/banana.jpg');
+var bathroom = new img('bathroom', 'assets/bathroom.jpg');
+var boots = new img('boots', 'assets/boots.jpg');
+var breakfast = new img('breakfast', 'assets/breakfast.jpg');
+var bubblegum = new img('bubblegum', 'assets/bubblegum.jpg');
+var chair = new img('chair', 'assets/chair.jpg');
+var cthulhu = new img('cthulhu', 'assets/cthulhu.jpg');
+var dogDuck = new img('dog-duck', 'assets/dog-duck.jpg');
+var dragon = new img('dragon', 'assets/dragon.jpg');
+var pen = new img('pen', 'assets/pen.jpg');
+var petSweep = new img('pet-sweep', 'assets/pet-sweep.jpg');
+var scissors = new img('scissors', 'assets/scissors.jpg');
+var shark = new img('shark', 'assets/shark.jpg');
+var sweep = new img('sweep', 'assets/sweep.png');
+var tauntaun = new img('tauntaun', 'assets/tauntaun.jpg');
+var unicorn = new img('unicorn', 'assets/unicorn.jpg');
+var usb = new img('usb', 'assets/usb.gif');
+var waterCan = new img('water-can', 'assets/water-can.jpg');
+var wineGlass = new img('wine-glass', 'assets/wine-glass.jpg');
+
+randomImg();
+// console.log(imgArr);
+// for(var i = 0; i < 25; i++) {
+//   imgArr[i].imgChoices();
+// };
